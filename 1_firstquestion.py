00331edc19127aca97f85e008049188ec768dd71
Dylan-Barnett <D534992010@gmail.com>
2024-07-30 13:31:21 -0500

    initial commit

---
+++ 1_firstquestion.py
@@ -0,0 +1,15 @@
+weather= "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:   
+    print("Take an umbrella!")
+
+user_input= input("how are you feeling today?")
+print(user_input)
+print(type(user_input))
+if user_input == "happy":
+    print("Thats's great to hear!")
+if user_input == "sad":
+    print("I hope your day gets better!")
+
+
---
+++ 3_Arithmetic_operations.py
@@ -0,0 +1,4 @@
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+print( bread + peanut_butter + jelly)
